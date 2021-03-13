94e4617246fd6e0589436920f0c49f43dcfbaba6
Shubhankar Chaudhury <shubhankar1991@gmail.com>
2021-03-13 17:41:55 +0530

    Added Files - Cortex M4 drive header - Cortex M4 NVIC implementation (h/c)

---
+++ Source/Driver/Inc/cortex_m4_driver.h
@@ -0,0 +1,25 @@
+/***************************************************************************************************
+ * @file      cortex_m4_driver.h
+ * @brief     Cortex M4 CPU core driver header file.
+ *            
+ * @author    Shubhankar Chaudhury
+ * @date      13 Mar 2021
+ **************************************************************************************************/
+ 
+ #ifndef CORTEX_M4_DRIVER_H_
+ #define CORTEX_M4_DRIVER_H_
+ 
+ 
+// Includes ---------------------------------------------------------------------------------------
+
+#include <stdint.h>
+#include <stddef.h>
+
+// Defines ----------------------------------------------------------------------------------------
+
+//Common
+#define FALSE     0x00
+#define TRUE      0x01
+
+ 
+#endif /* CORTEX_M4_DRIVER_H_ */
---
+++ Source/Driver/Inc/cortex_m4_nvic.h
@@ -0,0 +1,13 @@
+/***************************************************************************************************
+ * @file      cortex_m4_nvic.h
+ * @brief     Cortex M4 CPU core NVIC header file.
+ *            
+ * @author    Shubhankar Chaudhury
+ * @date      13 Mar 2021
+ **************************************************************************************************/
+ 
+ #ifndef CORTEX_M4_NVIC_H_
+ #define CORTEX_M4_NVIC_H_
+ 
+ 
+ #endif /* CORTEX_M4_NVIC_H_ */
