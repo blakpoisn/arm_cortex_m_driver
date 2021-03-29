853572accf6721c75d10ce76cadbe5a58228cdcb
Shubhankar Chaudhury <shubhankar1991@gmail.com>
2021-03-29 16:02:07 +0530

    Small Fix  - added volatile to nvic_regdef_t components

--- Source/Driver/Inc/cortex_m4_driver.h
+++ Source/Driver/Inc/cortex_m4_driver.h
@@ -3,7 +3,7 @@
  * @brief     Cortex M4 CPU core driver header file.
  *            
  * @author    Shubhankar Chaudhury
- * @date      13 Mar 2021
+ * @date      29 Mar 2021
  **************************************************************************************************/
  
  #ifndef CORTEX_M4_DRIVER_H_
@@ -21,5 +21,30 @@
 #define FALSE     0x00
 #define TRUE      0x01
 
- 
+//Core peripherals
+#define MA_NVIC_BEG       0xE000E100U     /*!< Nested Vector Interrupt Controller Memory Region. */
+#define MA_NVIC_STIR      0xE000EF00U     /*!< NVIC Software trigger interrupt register. */
+
+// Structures --------------------------------------------------------------------------------------
+/**
+ *  @brief Register deffinition for NVIC CPU core memory map.
+ */
+typedef struct {
+  volatile uint32_t ISER[8],       /*!< Interrupt Set Enable Register. */
+  volatile uint32_t RESa[24],      /*!< Reserved. */
+  volatile uint32_t ICER[8],       /*!< Interrupt Clear Enable Register. */
+  volatile uint32_t RESb[24],      /*!< Reserved. */
+  volatile uint32_t ISPR[8],       /*!< Interrupt Set Pending Register. */
+  volatile uint32_t RESc[24],      /*!< Reserved. */
+  volatile uint32_t ICPR[8],       /*!< Interrupt Clear Pending Register. */
+  volatile uint32_t RESd[24],      /*!< Reserved. */
+  volatile uint32_t IABR[8],       /*!< Interrupt Active Bit Register. */
+  volatile uint32_t RESe[56],      /*!< Reserved. */
+  volatile uint32_t IPR[60]        /*!< Interrupt Priority Register. */
+} nvic_regdef_t;
+
+// Peripheral Definitions --------------------------------------------------------------------------
+#define NVIC_MEM_MAP     ((nvic_regdef_t *)MA_NVIC_BEG)        /*!< NVIC register memory map access pointer. */
+
+
 #endif /* CORTEX_M4_DRIVER_H_ */
